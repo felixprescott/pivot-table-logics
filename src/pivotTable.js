2b1016814bbec1544a615580d6155c77ec2eac15
Felix Prescott <info@felixprescott.com>
2021-09-01 18:57:52 +0400

    добавлен функционал разворачивания и сворачивания групп

--- src/pivotTable.js
+++ src/pivotTable.js
@@ -1,4 +1,10 @@
+import { useState, useEffect } from 'react';
+
 const PivotTable = ( {data, selectedFields} ) => {
+
+  const initSettings = {};
+  const [openSettings, setOpenSettings] = useState(initSettings);
+  
   const tableStyle = {
     borderCollapse: 'collapse',
     width: '800px',
@@ -16,7 +22,30 @@ const PivotTable = ( {data, selectedFields} ) => {
     backgroundColor: 'lavender',
     fontWeight: 'bold',
   };
+
+  const makeKeyFromHeader = (rowHeader) => {
+    return rowHeader.map(e=>e.fieldValue).join('/');
+  }
   
+  const switchFolderOpened = (key) => {
+    const settings = {...openSettings}
+    settings[key] = !openSettings[key];
+    setOpenSettings(settings);
+  };
+
+  useEffect(() => {
+    if(data.rows.length) {
+      const settings = {};
+      for (let i=0; i<data.rows.length; i++){
+        if(data.rows[i].rowHeader.length<selectedFields.length) {
+          settings[makeKeyFromHeader(data.rows[i].rowHeader)] = false;
+        }
+      }
+      
+      setOpenSettings(settings);
+    }    
+  },[data, selectedFields]);
+
   return (
     <table style={tableStyle}>
       <thead>
@@ -27,15 +56,27 @@ const PivotTable = ( {data, selectedFields} ) => {
       </thead>
       <tbody>
         {data.rows.map( (row) => {
-          return (
-            <tr>
-              <td style={{border: '1px solid gray', backgroundColor: 'aliceblue', paddingLeft: row.margin*20+'px'}}>
-                { (row.rowHeader.length<selectedFields.length) && <button>-</button>}
-                {row.rowHeader[row.rowHeader.length-1].fieldValue}
-              </td>
-              {row.cells.map( cell => <td style={t}>{cell}</td>)}
-            </tr>
-          )
+          let isRowShowing = true;
+          if (row.rowHeader.length>1) {
+            for (let i=0; i<row.rowHeader.length-1; i++) {
+              isRowShowing = isRowShowing && openSettings[makeKeyFromHeader(row.rowHeader.slice(0,i+1))];
+            }
+          } else {isRowShowing = true}
+          if (isRowShowing)
+            return (
+              <tr>
+                <td style={{border: '1px solid gray', backgroundColor: 'aliceblue', paddingLeft: row.margin*20+'px'}}>
+                  {
+                    (row.rowHeader.length<selectedFields.length) && 
+                      <button onClick={() => switchFolderOpened(makeKeyFromHeader(row.rowHeader))}>
+                        { (openSettings[makeKeyFromHeader(row.rowHeader)]) ? '–' : '+' }
+                      </button>
+                  }
+                  {row.rowHeader[row.rowHeader.length-1].fieldValue}
+                </td>
+                {row.cells.map( cell => <td style={t}>{cell}</td>)}
+              </tr>
+            )
         })}
       </tbody>
       <tfoot>
